bb711145e5e33b81f202702539c7a636adb06728
Shirasawa <yrry.nrm@gmail.com>
2024-10-12 17:13:40 +0900

    ✨ カウンタの実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,33 @@ import { Footer } from "@/src/components/Footer";
 import { Headline } from "@/src/components/Headline";
 import { Links } from "@/src/components/Links";
 import { useCallback } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   const handleClick = useCallback((e) => {
     console.log(e);
-    alert('クリック');
+    setCount(count => count + 1);
+  }, []);
+  
+  useEffect(() => {
+    // マウント時の処理
+    document.body.style.backgroundColor = 'lightblue';
+
+    // アンマウント時の処理
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
   }, []);
 
   return (
     <div>
       <Header />
-      <button
-        onClick={() => {
-          alert('クリック')
-        }}>
-          クリック
-      </button>
-      <Headline title="Index Page" page="index" onClick={handleClick}/> 
+      <h3>{count}</h3>
+      <button onClick={handleClick}>カウントアップ</button>
+      <Headline title="Index Page" page="index" onClick={() => {alert('クリック')}}/> 
       <Footer />
       <Links />
     </div>
