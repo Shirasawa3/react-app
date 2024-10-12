9de5222531bad4c4accd391a8353c73eb4b69711
Shirasawa <yrry.nrm@gmail.com>
2024-10-12 16:32:47 +0900

    ✨ button追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,24 @@ import { Header } from "@/src/components/Header";
 import { Footer } from "@/src/components/Footer";
 import { Headline } from "@/src/components/Headline";
 import { Links } from "@/src/components/Links";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e);
+    alert('クリック');
+  }, []);
+
   return (
     <div>
-      <Header /> 
-      <Headline title="Index Page" page="index" onClick={() => alert('クリック')}/> 
+      <Header />
+      <button
+        onClick={() => {
+          alert('クリック')
+        }}>
+          クリック
+      </button>
+      <Headline title="Index Page" page="index" onClick={handleClick}/> 
       <Footer />
       <Links />
     </div>
